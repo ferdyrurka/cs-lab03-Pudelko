582da332b51b5c9c8b54505abc7114557bfba3b6
Staniszewski Łukasz <lukasz.staniszewski@microsoft.wsei.edu.pl>
2020-11-15 19:09:32 +0100

    end

---
+++ PudelkoConsoleApp/PudelkoExt.cs
@@ -0,0 +1,16 @@
+﻿using PudelkoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PudelkoConsoleApp
+{
+    public static class PudelkoExt
+    {
+        public static Pudelko Kompresuj(this Pudelko pudelko)
+        {
+            double size = Math.Cbrt(pudelko.Objetosc);
+            return new Pudelko(size, size, size);
+        }
+    }
+}
